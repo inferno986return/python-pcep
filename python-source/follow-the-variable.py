d3a59f4a4f30040429b065dc5340d752657e338a
Hal Motley <inferno986returns@gmail.com>
2024-05-19 15:21:03 +0100

    fix lab 7 + follow the vaiiable

---
+++ python-source/follow-the-variable.py
@@ -0,0 +1,6 @@
+a = 3
+b = 4
+a, b, c = b, a, a
+b, a, c = b, c, a
+
+print(a, b, c)
--- python-source/my-examples/lab7-quadratic.py
+++ python-source/my-examples/lab7-quadratic.py
@@ -13,7 +13,9 @@
 # y = -9.0
 
 x = -1 # Hardcode your test data here.
+print(f"x = {x}")
 x = float(x)
+
 y = 3*x**3 - 2*x**2 + 3*x - 1 # Write your code here.
-print("y =", y)
+print(f"y = {y}")
  
